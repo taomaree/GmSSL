2a11b9a208f6b4c3fad36bdba989b15e7d04bf22
段杰 <sdir@users.noreply.github.com>
2021-10-20 16:56:05 +0800

    fix tls1.0 bad record mac

--- ssl/s3_lib.c
+++ ssl/s3_lib.c
@@ -4222,7 +4222,7 @@ long ssl_get_algorithm2(SSL *s)
     } else{
     // in the end of finish msg of gmssl handshake, to get the correct hash algo
         printf("ssl_get_algorithm2=%0x08x\n", ssl_get_algorithm2);
-        if (alg2 == 0x909){
+        if (SSL_IS_GMTLS(s) && alg2 == (SSL_HANDSHAKE_MAC_DEFAULT | TLS1_PRF)){
             return SSL_HANDSHAKE_MAC_SM3 | TLS1_PRF_SM3;
         }
     }
